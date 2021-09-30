aea6cbafafdbde93101ab8c0d559e4c12015440f
Hoa Nguyen <hoaannguyen07@gmail.com>
2021-09-29 20:21:56 -0500

    Delete app directory

--- app/models/book.rb
+++
@@ -1,7 +0,0 @@
-class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :price, presence: true
-    validates :price, numericality: {only_float: true}
-    validates :published_date, presence: true
-end
